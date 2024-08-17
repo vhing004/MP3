a24319ae1bed6827967129e81f7f1b644cca9449
vhing004 <hvinh12112004@gmail.com>
2024-08-17 10:04:35 +0700

    prev & next & animate rotate & play & pause & tua

--- app.js
+++ app.js
@@ -1,56 +1,51 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const btnPlay = $(".btn-toggle-play");
+const player = $(".player");
+const progress = $("#progress");
+const nextBtn = $(".btn-next");
+const prevBtn = $(".btn-prev");
 const app = {
+  currentIndex: 0,
+  isPlay: false,
   songs: [
     {
-      name: "Click Pow Get Down",
-      singer: "Raftaar x Fortnite",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
-      image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg",
+      name: "Em Bên Ai Rồi",
+      singer: "Thành Đạt",
+      path: "./assets/audio/embenairoi.mp3",
+      image: "./assets/image/img_1.jpg",
     },
     {
-      name: "Tu Phir Se Aana",
-      singer: "Raftaar x Salim Merchant x Karma",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
-      image:
-        "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg",
+      name: "Từng Yêu",
+      singer: "Phan Duy Anh",
+      path: "./assets/audio/tungyeu.mp3",
+      image: "./assets/image/img_2.jpg",
     },
     {
-      name: "Naachne Ka Shaunq",
-      singer: "Raftaar x Brobha V",
-      path: "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
-      image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg",
+      name: "Anh Thôi Nhân Nhượng",
+      singer: "Kiều Chi",
+      path: "./assets/audio/anhthoinhannhuong.mp3",
+      image: "./assets/image/img_3.jpg",
     },
     {
-      name: "Mantoiyat",
-      singer: "Raftaar x Nawazuddin Siddiqui",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
-      image:
-        "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg",
+      name: "Cẩm Tú Cầu",
+      singer: "RayO",
+      path: "./assets/audio/camtucau.mp3",
+      image: "./assets/image/img_4.jpg",
     },
     {
-      name: "Aage Chal",
-      singer: "Raftaar",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
-      image:
-        "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg",
-    },
-    {
-      name: "Damn",
-      singer: "Raftaar x kr$na",
-      path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
-      image:
-        "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg",
-    },
-    {
-      name: "Feeling You",
-      singer: "Raftaar x Harjas",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
-      image:
-        "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp",
+      name: "Nên Chờ Hay Nên Quên",
+      singer: "Chu Thúy Quỳnh",
+      path: "assets/audio/nenchohayquen.mp3",
+      image: "./assets/image/img_5.jpg",
     },
   ],
+
+  //render playlist
   render: function () {
     var htmls = this.songs.map((song) => {
       return `
@@ -70,19 +65,121 @@ const app = {
     });
     $(".playlist").innerHTML = htmls.join("");
   },
+
+  // định nghĩa obj
+  defineProperties: function () {
+    Object.defineProperty(this, "currentSong", {
+      get: function () {
+        return this.songs[this.currentIndex];
+      },
+    });
+  },
+
+  // load ra bài đầu tiên
+  loadCurrentSong: function () {
+    heading.textContent = this.currentSong.name;
+    cdThumb.style.backgroundImage = `url("${this.currentSong.image}")`;
+    audio.src = this.currentSong.path;
+    console.log(audio.src);
+  },
+
+  // handle events
   handleEvent: function () {
+    const _this = this;
     const cdWidth = $(".cd").offsetWidth;
-    
+
+    // animate rotate
+    const cdThumbRotate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+      duration: 10000,
+      iterations: Infinity,
+    });
+    cdThumbRotate.pause();
+
+    // xử lý phóng to thu nhỏ mà mờ dần khi cuộn
     document.addEventListener("scroll", () => {
       const scrollPage = window.scrollY || document.documentElement.scrollTop;
       const widthCd = cdWidth - scrollPage;
-
       $(".cd").style.width = widthCd > 0 ? widthCd + "px" : 0;
       $(".cd").style.opacity = widthCd / cdWidth;
     });
+
+    // xử lý click play
+    btnPlay.onclick = function () {
+      if (_this.isPlay) {
+        audio.pause();
+        cdThumbRotate.pause();
+      } else {
+        audio.play();
+        cdThumbRotate.play();
+      }
+    };
+
+    // khi song đc PLAY:
+    audio.onplay = function () {
+      player.classList.add("playing");
+      _this.isPlay = true;
+    };
+
+    // khi song đc PAUSE:
+    audio.onpause = function () {
+      player.classList.remove("playing");
+      _this.isPlay = false;
+    };
+
+    // update time when audio play
+    audio.ontimeupdate = function () {
+      if (this.duration) {
+        const progressPercent = Math.floor(
+          (this.currentTime / this.duration) * 100
+        );
+        //duration là lấy thời gian audio
+        progress.value = progressPercent;
+      }
+    };
+
+    // Tua audio
+    progress.oninput = function (e) {
+      const seekTime = (audio.duration / 100) * e.target.value;
+      audio.currentTime = seekTime;
+      //currentTime can get time of audio & Set time cho audio
+    };
+
+    // next
+    nextBtn.onclick = function () {
+      _this.next();
+      audio.play()
+    };
+
+    // prev
+    prevBtn.onclick = function () {
+      _this.prev();
+      audio.play()
+    };
+  },
+  next: function () {
+    this.currentIndex++;
+    if (this.currentIndex >= this.songs.length) {
+      this.currentIndex = 0;
+    }
+    this.loadCurrentSong();
+  },
+  prev: function () {
+    this.currentIndex--;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.songs.length - 1;
+    }
+    this.loadCurrentSong();
   },
   start: function () {
+    // định nghĩa các thuộc tính của obj
+    this.defineProperties();
+
+    // xử lý sự kiện
     this.handleEvent();
+
+    this.loadCurrentSong();
+
+    // render playlist
     this.render();
   },
 };
